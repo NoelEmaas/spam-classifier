687e29751f6355ae94e6e8affdf3114a7ee4a9b2
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-08-30 09:22:22 +0800

    added project files

---
+++ src/main.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import re
+from nltk.tokenize import word_tokenize
+from naive_bayes_spam_classifier import NaiveBayesSpamClassifier
+
+
+def main ():
+    # Read the data
+    training_data = pd.read_csv('../data/TrainingData.csv', encoding='ISO-8859-1')
+    testing_data = pd.read_csv('../data/TestData.csv', encoding='ISO-8859-1')
+
+    # Clean the data
+    training_data = cleanData(training_data)
+    testing_data = cleanData(testing_data)
+
+    # Create a NaiveBayesSpamClassifier object
+    classifier = NaiveBayesSpamClassifier()
+
+    # Train the classifier
+    classifier.train(training_data)
+
+    # Test the classifier
+    classifier.test(testing_data)
+
+
+def cleanData (data):
+    data = data.drop_duplicates()
+    data = data.dropna()
+
+    def clean_message(message):
+        message = message.lower()
+        message = re.sub(r'[^\w\s]', '', message)
+        words = word_tokenize(message)
+        words = [word for word in words if word.isalpha()]
+        cleaned_message = ' '.join(words)
+        return cleaned_message
+
+    data['message'] = data['message'].apply(clean_message)
+    return data
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/naive_bayes_spam_classifier.py
@@ -0,0 +1,71 @@
+import re
+from nltk.tokenize import word_tokenize
+from collections import defaultdict, Counter
+
+
+class NaiveBayesSpamClassifier:
+    def __init__ (self):
+        self.vocabulary = set()
+        self.total_number_of_words = 0
+        self.spam_word_probabilities = defaultdict()
+        self.ham_word_probabilities = defaultdict()
+
+    # Train the classifier
+    def train (self, training_data):
+
+        # - list of distinct words from entire data set
+        # - list of distinct words from spam messages
+        # - list of distinct words from ham messages
+        # - count of each word in spam messages
+        # - count of each word in ham messages'
+
+        print('Training the classifier...')
+
+        spam_word_counts = Counter()
+        ham_word_counts = Counter()
+        spam_words = set()
+        ham_words = set()
+
+        for _, row in training_data.iterrows():
+            message = row['message']
+            label = row['label']
+
+            words = word_tokenize(message)
+            self.total_number_of_words += len(words)
+            self.vocabulary.update(words)
+
+            if label == 'spam':
+                spam_word_counts.update(words)
+                spam_words.update(words)
+            else:
+                ham_word_counts.update(words)
+                ham_words.update(words)
+        
+        # Get the probability of each word in spam and ham messages
+        for word in self.vocabulary:
+            self.spam_word_probabilities[word] = (spam_word_counts[word] + 1) / (len(spam_words) + len(self.vocabulary))
+            self.ham_word_probabilities[word] = (ham_word_counts[word] + 1) / (len(ham_words) + len(self.vocabulary))
+
+        print("Training complete!\n")
+
+    # Predict the label of a message based on trainging data
+    def test (self, test_data):
+        print('Classification Result:')
+
+        for idx, row in test_data.iterrows():
+            probability_spam = 1
+            probability_ham = 1
+
+            message = row['message']
+            words = word_tokenize(message)
+
+            smoothing_factor = 1 / (self.total_number_of_words + len(self.vocabulary) + 1)
+
+            for word in words:
+                probability_spam *= self.spam_word_probabilities.get(word, smoothing_factor)
+                probability_ham *= self.ham_word_probabilities.get(word, smoothing_factor)
+
+            print("[SPAM] " + message) if probability_spam > probability_ham else print("[HAM] " + message)
+            
+            if idx == 10:
+                break
